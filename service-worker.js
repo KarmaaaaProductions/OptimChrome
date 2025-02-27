fa5845df50a0c21921fe0750e27ca1068042d4e5
ChromebookUser2025 <30anandkr@fpsct.org>
2025-02-27 14:44:56 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("optimchrome-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/app.js",
+        "/manifest.json",
+        "/icon-192.png",
+        "/icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
